c9ed2665b8a1c240a4fd85a58383a1ad5d97f2de
Randy <drbugsmn@gmail.com>
2016-03-08 10:59:03 -0600

    tweeks to use git repo on github

--- src/main/java/com/dood/config/SpringConfigServerMicroSvcApplication.java
+++ src/main/java/com/dood/config/SpringConfigServerMicroSvcApplication.java
@@ -4,6 +4,10 @@ import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.cloud.config.server.EnableConfigServer;
 
+/**
+ * This is the main springboot application class that will fire up spring config server.  There really is nothing
+ * to it, the real magic is the @EnableConfigServer
+ */
 @SpringBootApplication
 @EnableConfigServer
 public class SpringConfigServerMicroSvcApplication {
